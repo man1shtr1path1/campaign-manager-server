980b029857861f21237738fd7343a2dc525365d1
man1shtr1path1 <manishtripathi10041998@outlook.com>
2024-07-05 15:42:02 +0530

    create routes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const campaignRoutes = require('./routes/campaigns');
+const app = express();
+const PORT = 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/campaigns', require('./routes/campaigns'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
